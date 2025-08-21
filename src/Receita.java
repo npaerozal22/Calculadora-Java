7e1887401f09d867b87082019beb5d37ffdf0215
Natanael Rozal <96591757+npaerozal22@users.noreply.github.com>
2025-08-20 22:35:20 -0300

    Create Receita.java

---
+++ src/Receita.java
@@ -0,0 +1,17 @@
+public class Receita {
+    private double valor;
+    private String descricao;
+
+    public Receita(double valor, String descricao) {
+        this.valor = valor;
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
